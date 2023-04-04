6cd8e28e857a64fb0309ed735b773246730bcbe1
Levi Buzolic <levibuzolic@gmail.com>
2023-04-04 13:30:49 +1000

    Apply prettier

--- src/cli-parameters.ts
+++ src/cli-parameters.ts
@@ -1,21 +1,21 @@
-import minimist from 'minimist'
-import { createError, ErrorCode } from './errors'
+import minimist from 'minimist';
+import { createError, ErrorCode } from './errors';
 import tempfile from 'tempfile';
 
-export type Platform = 'ios' | 'android'
+export type Platform = 'ios' | 'android';
 
 /**
  * The parameters passed in from the command line.
  */
 export interface CliParameters {
   /** Will we be using ios or android? */
-  platform?: Platform
+  platform?: Platform;
   /** A file name to save the image. */
-  filename: string
+  filename: string;
   /** An optional device name for if more than 1 device is active. */
-  device?: string
+  device?: string;
   /** Should we use the clipboard? */
-  useClipboard: boolean
+  useClipboard: boolean;
 }
 
 /**
@@ -24,17 +24,17 @@ export interface CliParameters {
  * @param parsed The minimist parsed arguments.
  */
 function getPlatform(parsed: minimist.ParsedArgs) {
-  const platform = parsed._[0] as string
+  const platform = parsed._[0] as string;
 
   // no platform?
-  if (!platform) throw createError(ErrorCode.MissingPlatform)
+  if (!platform) throw createError(ErrorCode.MissingPlatform);
 
   // bad platform?
   if (platform !== 'ios' && platform !== 'android') {
-    throw createError(ErrorCode.InvalidPlatform)
+    throw createError(ErrorCode.InvalidPlatform);
   }
 
-  return platform
+  return platform;
 }
 
 /**
@@ -44,16 +44,16 @@ function getPlatform(parsed: minimist.ParsedArgs) {
  */
 export function parse(argv?: string[]): CliParameters {
   // use the default process args if nothing was passed in
-  argv = argv || process.argv.slice(2)
+  argv = argv || process.argv.slice(2);
 
   // parse the arguments with minimist
-  const parsed = minimist(argv)
+  const parsed = minimist(argv);
 
   // grab what we need
-  const platform = getPlatform(parsed)
-  const useClipboard = !parsed['f']
+  const platform = getPlatform(parsed);
+  const useClipboard = !parsed['f'];
   const filename = (parsed['f'] as string) || tempfile('.png');
-  const device = parsed['d'] as string
+  const device = parsed['d'] as string;
 
-  return { platform, filename, device, useClipboard }
+  return { platform, filename, device, useClipboard };
 }
