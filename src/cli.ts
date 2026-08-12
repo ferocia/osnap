6ae6ac70c56364cd6d550c69c63c764e29629ce3
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2026-08-13 09:46:48 +1000

    Test cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,39 @@
-import { parse } from './cli-parameters.js';
-import { type OsnapError, ErrorCode } from './errors.js';
-import { usage } from './cli-usage.js';
-import { saveToFile as iosSave } from './ios.js';
-import { saveToFile as androidSave } from './android.js';
-import { copyToClipboard } from './copy-to-clipboard.js';
+import { parse } from './cli-parameters.js'
+import { type OsnapError, ErrorCode } from './errors.js'
+import { usage } from './cli-usage.js'
+import { saveToFile as iosSave } from './ios.js'
+import { saveToFile as androidSave } from './android.js'
+import { copyToClipboard } from './copy-to-clipboard.js'
 
 /**
  * Runs the CLI and returns the exit code we should use.
  */
 export async function run() {
   try {
-    const parameters = parse();
+    const parameters = parse()
 
     // save it to a file
     if (parameters.platform === 'ios') {
-      await iosSave(parameters);
+      await iosSave(parameters)
     } else if (parameters.platform === 'android') {
-      await androidSave(parameters);
+      await androidSave(parameters)
     }
 
     // copy it to the clipboard
     if (parameters.useClipboard) {
-      await copyToClipboard(parameters.filename);
+      await copyToClipboard(parameters.filename)
     }
 
-    return 0;
+    return 0
   } catch (err) {
     // typescript doesn't support typing the catch parameter
     if ((err as OsnapError).code === ErrorCode.MissingPlatform) {
       // print usage instead
-      console.log(usage);
-      return 0;
+      console.log(usage)
+      return 0
     } else {
-      console.error((err as Error).message);
-      return 1;
+      console.error((err as Error).message)
+      return 1
     }
   }
 }
